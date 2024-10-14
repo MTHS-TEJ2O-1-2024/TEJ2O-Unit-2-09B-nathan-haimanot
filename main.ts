31fb3bee5f9bb8d957f5b190c058ed216d726f0e
nathan-haimanot <nathan.haimanot@mths.ca>
2024-10-14 13:31:09 -0400

    rock paper scissors

--- main.ts
+++ main.ts
@@ -2,7 +2,50 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program does rock paper scissor
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = 0
+let score = 0
+
+randomNumber = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+
+    // rock
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+
+    // paper
+    if (randomNumber == 1) {
+        basic.showIcon(IconNames.Square)
+    }
+
+    // scissor
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+    
+    }
+    basic.pause(1000)
+
+    
+
+})
+
+// checkmark
+    input.onButtonPressed(Button.A, function () {
+        score += 1
+        basic.showIcon(IconNames.Happy)
+        basic.showIcon(IconNames.Yes)
+    })
+
+input.onButtonPressed(Button.B, function () {
+        basic.showString('score')
+        basic.showNumber(score)
+})
