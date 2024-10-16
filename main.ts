08a2fdaa7a93be511d5179410d32980d7112dc80
nathan-haimanot <nathan.haimanot@mths.ca>
2024-10-16 18:02:13 -0400

    changed name

--- main.ts
+++ main.ts
@@ -20,32 +20,34 @@ input.onGesture(Gesture.Shake, function () {
     // rock
     if (randomNumber == 0) {
         basic.showIcon(IconNames.SmallSquare)
+        basic,pause(1000)
     }
 
     // paper
     if (randomNumber == 1) {
         basic.showIcon(IconNames.Square)
+        basic.pause(1000)
     }
 
     // scissor
     if (randomNumber == 2) {
         basic.showIcon(IconNames.Scissors)
-    
-    }
+        basic.pause(1000)
+     }
     basic.pause(1000)
-
-    
-
 })
 
 // checkmark
     input.onButtonPressed(Button.A, function () {
         score += 1
-        basic.showIcon(IconNames.Happy)
         basic.showIcon(IconNames.Yes)
+        basic.pause(1000)
+        basic.showIcon(IconNames.Happy)
+        basic.pause(1000)
     })
 
 input.onButtonPressed(Button.B, function () {
-        basic.showString('score')
-        basic.showNumber(score)
+    basic.clearScreen()
+        basic.showString('score:' + score)
+        basic.showIcon(IconNames.Happy)
 })
